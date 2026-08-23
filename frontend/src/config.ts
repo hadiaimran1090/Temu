cd306ca3b72dc620c70b5e8418262681498ebfde
Hadia Imran <hadiaimran730@gmail.com>
2026-08-23 17:29:07 +0500

    commit

--- frontend/src/config.ts
+++
@@ -1,3 +0,0 @@
-// Read support contact from environment variables
-export const SUPPORT_WHATSAPP_NUMBER = import.meta.env.VITE_SUPPORT_WHATSAPP_NUMBER || "923001234567";
-export const SUPPORT_EMAIL = import.meta.env.VITE_SUPPORT_EMAIL || "hadiaimran027@gmail.com";
