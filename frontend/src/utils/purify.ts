b2e16d6ad6bb3e1ef98ca9037ac0a7a536085649
Hadia Imran <hadiaimran730@gmail.com>
2026-08-24 06:30:41 +0500

    commit

--- frontend/src/utils/purify.ts
+++ frontend/src/utils/purify.ts
@@ -2,8 +2,7 @@ import DOMPurify from "dompurify";
 
 /**
  * Sanitizes HTML only when the result will be rendered as HTML.
- *
- * Do not use this for plain-text values such as form fields or search queries:
+ 
  * React renders those values as text already, and sanitizing them as HTML can
  * turn characters such as `<` and `&` into entity strings.
  */
